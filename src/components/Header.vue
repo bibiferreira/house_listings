8868b879ca3ede8698fde990cd7da90f1a28fef3
Bibi Ferreira <bibi.espeiorin@gmail.com>
2022-07-25 23:08:05 +0200

    created the houses page with the elements for the search, added the fonts to the css and styled the nav menu.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -11,9 +11,23 @@ export default {
     <nav>
       <a href="/#"><img src="./icons/img_logo_dtt.png" /></a>
       <ul>
-        <li><a href="/#houses">Houses</a></li>
+        <li><a href="/#houses" class="active">Houses</a></li>
         <li><a href="/#about">About</a></li>
       </ul>
     </nav>
   </header>
 </template>
+
+<style scoped>
+
+nav li a.active {
+  font: var(--dtt-f-active-menu);
+  color: var(--color-menu-active);
+}
+
+nav li a {
+  font: var(--dtt-f-header-inactive-menu);
+  color: var(--color-menu-inactive);
+}
+
+</style>
--- src/components/Houses.vue
+++ src/components/Houses.vue
@@ -7,5 +7,60 @@ export default {
 }
 
 <template>
-  <div>Home</div>
+  <div>
+    <div class="houses-header">
+      <h2>Houses</h2>
+      <button class="primary-button"><img src="./icons/ic_plus_white.png"/><span>CREATE NEW</span></button>
+    </div>
+    <div class="houses-header">
+      <div class="input-wrapper">
+        <img src="./icons/ic_search.png" />
+        <input type="search" placeholder="Search for a house"/>
+      </div>
+      <div class="tab">
+        <button class="first-tab primary-button"><span>Price</span></button>
+        <button class="last-tab tertiary-button"><span>Size</span></button>
+      </div>
+    </div>
+    <div></div>
+  </div>
 </template>
+
+<style scoped>
+
+.houses-header {
+  display: flex;
+  justify-content: space-between;
+  padding-bottom: 30px;
+}
+
+.input-wrapper {
+  background-color: var(--color-input-background);
+  box-sizing: content-box;
+  padding: 2px 10px;
+  border-radius: 5px;
+  height: 30px;
+  display: flex;
+  align-items: stretch;
+  width: 320px;
+}
+
+.input-wrapper img {
+  width: 20px;
+  height: 20px;
+  margin-top: 5px;
+}
+
+.input-wrapper input {
+  border: none;
+  background: none;
+  height: 100%;
+  margin-left: 10px;
+  flex-grow: 2;
+}
+
+.input-wrapper input:focus {
+  outline: none;
+}
+
+</style>
